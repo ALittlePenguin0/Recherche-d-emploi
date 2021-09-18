7631f54ae0bf353cbe4bd0191d2a454f9f63fc39
ALittlePenguin0 <79764850+ALittlePenguin0@users.noreply.github.com>
2021-09-18 19:20:52 +0200

    Update code source.c

--- c++/porte-monnaie électronique/code source.c
+++ c++/porte-monnaie électronique/code source.c
@@ -2,54 +2,54 @@ void intro_data()
 {
     int i;
     	
-    # vérification de la taille
-    if (p3>MAXI)    # MAXI est la taille maximale
+    // vérification de la taille
+    if (p3>MAXI)   // MAXI est la taille maximale
 	{
-	    sw1=0x6c;	# code erreur correspondant à l'erreur de taille
-        sw2=MAXI;	# sw2 contient l'information de la taille correcte
+	    sw1=0x6c;  // code erreur correspondant à l'erreur de taille
+        sw2=MAXI;	// sw2 contient l'information de la taille correcte
 		return;
     }
     	
-	sendbytet0(ins);	# acquitement
+	sendbytet0(ins);	// acquitement
 
-	uint16_t data[p3];  # déclaration du tableau data à p3 cases
+	uint16_t data[p3];  // déclaration du tableau data à p3 cases
 	
 	for(i=0;i<p3;i++)
 	{
-           data[i]=recbytet0();     # le tableau data reçoit les octets
-           eeprom_write_block(data,&ee_solde,p3); # écriture des octets dans le solde via la fonction eeprom_write_block
+           data[i]=recbytet0();     // le tableau data reçoit les octets
+           eeprom_write_block(data,&ee_solde,p3); // écriture des octets dans le solde via la fonction eeprom_write_block
 	}
 	
-	taille=p3;  # la variable globale taille est de type uint16_t
-	sw1=0x90;   # code qui valide une bonne exécution de la fonction
+	taille=p3;  // la variable globale taille est de type uint16_t
+	sw1=0x90;   // code qui valide une bonne exécution de la fonction
 }
 
 void sortir_data()
 {
-	uint16_t data[p3];  # déclaration du tableau data à p3 cases
-    eeprom_read_block(data,&ee_solde,p3);   # data reçoit les valeurs de ee_solde
+	uint16_t data[p3];  // déclaration du tableau data à p3 cases
+    eeprom_read_block(data,&ee_solde,p3);   // data reçoit les valeurs de ee_solde
 
     int i;
     
 	# vérification de la taille
 	if (p3!=taille)
     	{
-        	sw1=0x6c;	# code erreur indiquant une taille incorrecte
-        	sw2=taille;	# sw2 prend la taille attendue
+        	sw1=0x6c;	// code erreur indiquant une taille incorrecte
+        	sw2=taille;	// sw2 prend la taille attendue
         	return;
     	}
 
-	sendbytet0(ins);	# acquittement
+	sendbytet0(ins);	// acquittement
 
-	# émission des données
+	// émission des données
 	for(i=0;i<p3;i++)
         	sendbytet0(data[i]);
-    sw1=0x90; # code qui valide une bonne exécution de la fonction
+    sw1=0x90; // code qui valide une bonne exécution de la fonction
 }
 
 void crediter(){
 
-    # vérification de la taille
+    // vérification de la taille
     if(p3!=taille)
     {
       sw1=0x6c;
@@ -57,32 +57,32 @@ void crediter(){
       return;
     }
     
-    # récupération des deux octets
+    // récupération des deux octets
     uint8_t x1=recbytet0(); 
     uint8_t x2=recbytet0();
     
-    # déclaration du solde 
+    // déclaration du solde 
     uint16_t solde;
     
-    # solde reçoit les valeurs de ee_solde
+    // solde reçoit les valeurs de ee_solde
     eeprom_read_block(&solde,&ee_solde,2); 
     
-    # addition des deux octets par méthode de décalage
+    // addition des deux octets par méthode de décalage
     uint16_t somme=(x1<<8) + x2; 
 
-   if(solde+somme>=solde)   # test de vérification, plus de détails dans la partie 3.4
+   if(solde+somme>=solde)   // test de vérification, plus de détails dans la partie 3.4
    {
-        solde=solde+somme;  # additioner solde et somme
+        solde=solde+somme;  // additioner solde et somme
         eeprom_write_block(&solde,&ee_solde,2); # mettre à jour le solde
-        sw1=0x90;   # code qui valide une bonne exécution de la fonction
+        sw1=0x90;   // code qui valide une bonne exécution de la fonction
     }
     else
-      sw1=0x6c; # code erreur
+      sw1=0x6c; // code erreur
 }
 
 void debiter(){
     
-    # premières instructions identiques à crediter()
+    // premières instructions identiques à crediter()
     
     if(p3!=taille)
     {
@@ -100,7 +100,7 @@ void debiter(){
     
     uint16_t somme=(x1<<8) + x2;
 
-    # faire la soustraction
+    // faire la soustraction
     if(solde-somme<=solde)  # test de vérification
     {
     solde = solde-somme; //soustraire somme à solde
